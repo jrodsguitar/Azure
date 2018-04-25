09414f0a6a173472a15dd8a1b328c5453e539c98
J Rodriguez <DAJose@answerfinancial.com>
2018-04-24 18:27:22 -0700

    commit

---
+++ webapp/deployAzureWebApp.ps1
@@ -0,0 +1,38 @@
+$secure = Import-Csv C:\me\github\secure\secure.csv
+
+
+if($azcred -eq $null){
+    $azcred = Get-Credential
+    }
+
+    $azTenId    = $secure.azTenId
+    $subscrname   = $secure.subscrname
+    $resourcegroup = $secure.resourcegroup
+    
+    if($azacct -eq $null){
+    $azacct = Login-AzureRmAccount -Subscription $subscrname
+    }
+    
+
+
+# Replace the following URL with a public GitHub repo URL
+$gitrepo="https://github.com/Azure-Samples/app-service-web-dotnet-get-started.git"
+$webappname="JoseWebApp$(Get-Random)"
+$location="West US"
+$appserviceplan = 'Default1'   
+
+
+# Create a web app.
+New-AzureRmWebApp -Name $webappname -Location $location -AppServicePlan $appserviceplan -ResourceGroupName $resourcegroup
+
+# Configure GitHub deployment from your GitHub repo and deploy once.
+$PropertiesObject = @{
+    repoUrl = "$gitrepo";
+    branch = "master";
+    isManualIntegration = "true";
+}
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $resourcegroup -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webappname/web -ApiVersion 2015-08-01 -Force
+
+
+
+    
