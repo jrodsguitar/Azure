824d938b228cafd55f9c8378140f7f3a0dcd69e6
jrodsguitar <jrodsguitar@gmail.com>
2018-04-27 15:00:30 -0700

    added deploy web app json template and github

---
+++ webapp/deployAzureWebAppFromGitHub.ps1
@@ -0,0 +1,47 @@
+$secure = Import-Csv C:\me\github\secure\secure.csv
+
+if($azcred -eq $null){
+    $azcred = Get-Credential
+    }
+
+    $azTenId    = $secure.azTenId
+    $subscrname   = $secure.subscrname
+    $resourcegroup = $secure.resourcegroup
+    
+    if($azacct -eq $null){
+    $azacct = Login-AzureRmAccount -Subscription $subscrname
+    }
+    
+# Replace the following URL with a public GitHub repo URL
+$json="https://raw.githubusercontent.com/jrodsguitar/Azure/master/webapp/deployDB.json"
+
+
+$webappname = "josewebapp$(Get-Random)"
+$appplanname = "appplanname$(Get-Random)"
+
+$pidguid = new-guid
+
+
+
+        $params =  @{
+        'TemplateURI' = "$json";
+        "webappname" = "$webappname";
+        "appplanname" = "$appplanname";
+        "repoURL" = "$repoURL";
+        "branch" = "$branch";
+        "pidguid" = "$pidguid";
+    
+        }
+
+
+#Deploy
+
+#test-AzureRmResourceGroupDeployment @params -ResourceGroupName $resourcegroup -ApiVersion 2014-04-01
+
+New-AzureRmResourceGroupDeployment  @params -ResourceGroupName $resourcegroup -ApiVersion 2014-01-01 -Force
+
+#$getwebappinfo = Get-AzureRmResource -ResourceGroupName $resourcegroup
+
+#Remove-AzureRmResource -ResourceGroupName $resourcegroup -ResourceType $getwebappinfo.ResourceType -ResourceName $resourcename-ApiVersion 2015-08-01 -Force
+
+    
