79a01a32c60d9c035418540563b60f08dca4adb1
jrodsguitar <jrodsguitar@gmail.com>
2018-04-27 01:02:19 -0700

    updated

--- webapp/deployAzureResource.ps1
+++ webapp/deployAzureResource.ps1
@@ -1,6 +1,5 @@
 $secure = Import-Csv C:\me\github\secure\secure.csv
 
-
 if($azcred -eq $null){
     $azcred = Get-Credential
     }
@@ -13,9 +12,6 @@ if($azcred -eq $null){
     $azacct = Login-AzureRmAccount -Subscription $subscrname
     }
     
-
-
-
 # Replace the following URL with a public GitHub repo URL
 $json="https://raw.githubusercontent.com/jrodsguitar/Azure/master/webapp/deployDB.json"
 
@@ -26,7 +22,6 @@ $sqlserverAdminLogin = "sqladmin"
 $password = Read-Host -assecurestring "Set DB password. Enter a password."
 $sqlserverAdminPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 $sqlservername = "josedbserv$(Get-Random)"
-
 
         $params =  @{
         'TemplateURI' = "$json";
@@ -40,22 +35,11 @@ $sqlservername = "josedbserv$(Get-Random)"
 
 #Deploy
 
-                
-                 
-# DELETE SB-JoseVM
-#Remove-AzureRmResource -ResourceGroupName SANDBOX_SRV_SERVERS -ResourceType Microsoft.Compute/virtualMachines -ResourceName "SB-JoseVM" -ApiVersion 2017-12-01 -Force
-    
-# DELETE SB-JoseVMNic
-#Remove-AzureRmResource -ResourceGroupName SANDBOX_SRV_SERVERS -ResourceType Microsoft.Network/networkInterfaces -ResourceName "SB-JoseVMNic" -ApiVersion 2017-03-01 -Force
-    
-
 #test-AzureRmResourceGroupDeployment @params -ResourceGroupName $resourcegroup -ApiVersion 2014-04-01
-
 
 New-AzureRmResourceGroupDeployment  @params -ResourceGroupName $resourcegroup -ApiVersion 2014-01-01 -Force
 
-
-#$getwebappinfo = Get-AzureRmResource -ResourceGroupName $resourcegroup -ResourceName $resourcename
+#$getwebappinfo = Get-AzureRmResource -ResourceGroupName $resourcegroup
 
 #Remove-AzureRmResource -ResourceGroupName $resourcegroup -ResourceType $getwebappinfo.ResourceType -ResourceName $resourcename-ApiVersion 2015-08-01 -Force
 
