8ec89441536ef8abe0fb373f82422a218eff8d59
jrodsguitar <jrodsguitar@gmail.com>
2018-04-27 17:49:31 -0700

    a

---
+++ webapp/deployAzureWebAppFromGitHubAAD.ps1
@@ -0,0 +1,45 @@
+$secure = Import-Csv C:\me\github\secure\secure.csv
+
+if($azcred -eq $null){
+    $azcred = Get-Credential
+    }
+
+    $azTenId    = $secure.azTenId
+    $subscrname   = $secure.subscrname
+    $resourcegroup = $secure.resourcegroup
+    
+    if($azacct -eq $null){
+    $azacct = Login-AzureRmAccount -Subscription $subscrname
+    }
+    
+# Replace the following URL with a public GitHub repo URL
+$json="https://raw.githubusercontent.com/jrodsguitar/Azure/master/webapp/deployWebApp.json"
+
+
+$webappname = "josewebapp$(Get-Random)"
+$appplanname = "joseappplanname$(Get-Random)"
+$repourl = "https://github.com/jrodsguitar/jrodsguitar-app-service-web-dotnet-get-started-master.git"
+$branch = "master"
+
+
+        $params =  @{
+        'TemplateURI' = "$json";
+        "webappname" = "$webappname";
+        "appplanname" = "$appplanname";
+        "repoURL" = "$repoURL";
+        "branch" = "$branch";
+    
+        }
+
+
+#Deploy
+
+#test-AzureRmResourceGroupDeployment @params -ResourceGroupName $resourcegroup -ApiVersion 2014-04-01
+
+New-AzureRmResourceGroupDeployment  @params -ResourceGroupName $resourcegroup -ApiVersion 2014-01-01 -Force
+
+#$getwebappinfo = Get-AzureRmResource -ResourceGroupName $resourcegroup
+
+#Remove-AzureRmResource -ResourceGroupName $resourcegroup -ResourceType $getwebappinfo.ResourceType -ResourceName $resourcename-ApiVersion 2015-08-01 -Force
+
+    
