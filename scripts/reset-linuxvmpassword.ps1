0d88f809f31ab69c852da0e2e3ea4d69e74785ac
jrodsguitar <>
2020-02-26 02:50:18 -0800

    added linux vm reset example

---
+++ scripts/reset-linuxvmpassword.ps1
@@ -0,0 +1,23 @@
+
+#Reset Linux VM password example
+$vm = get-azvm
+
+$linuxExtensionParams = @{
+
+'VMName' = $vm.Name
+
+'ResourceGroupName' = $vm.ResourceGroupName
+
+'Name' = 'VMAccessForLinux'
+
+'Location' = $vm.Location
+
+'Type' = 'VMAccessForLinux'
+
+'TypeHandlerVersion' = 1.4
+
+'Publisher' = 'Microsoft.OSTCExtensions'
+
+}
+
+Set-AzVMExtension @linuxExtensionParams -ProtectedSettings @{'password' = $pass; 'username' = $userName}
