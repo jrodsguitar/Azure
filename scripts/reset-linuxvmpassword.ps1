6cf0923a750757ef702f59ba6437e92e5963d3e2
jrodsguitar <>
2020-02-26 02:55:40 -0800

    Cleaned up format

--- scripts/reset-linuxvmpassword.ps1
+++ scripts/reset-linuxvmpassword.ps1
@@ -4,20 +4,14 @@ $vm = get-azvm
 
 $linuxExtensionParams = @{
 
-'VMName' = $vm.Name
-
-'ResourceGroupName' = $vm.ResourceGroupName
-
-'Name' = 'VMAccessForLinux'
-
-'Location' = $vm.Location
-
-'Type' = 'VMAccessForLinux'
-
-'TypeHandlerVersion' = 1.4
-
-'Publisher' = 'Microsoft.OSTCExtensions'
+    'VMName'             = $vm.Name
+    'ResourceGroupName'  = $vm.ResourceGroupName
+    'Name'               = 'VMAccessForLinux'
+    'Location'           = $vm.Location
+    'Type'               = 'VMAccessForLinux'
+    'TypeHandlerVersion' = 1.4
+    'Publisher'          = 'Microsoft.OSTCExtensions'
 
 }
 
-Set-AzVMExtension @linuxExtensionParams -ProtectedSettings @{'password' = $pass; 'username' = $userName}
+Set-AzVMExtension @linuxExtensionParams -ProtectedSettings @{'password' = $pass; 'username' = $userName }
